6f140ebe69b9e5b68c68d16e7c1929a734acaf47
lev <barpas@zoho.com>
2016-12-30 16:41:29 +0100

    simple http call

--- src/hello/core.cljs
+++ src/hello/core.cljs
@@ -1,14 +1,27 @@
 (ns hello.core
-  (:require ))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]] ))
 
 (enable-console-print!)
 
 (defn by-id [id]
   (js/document.getElementById id))
 
+;(def endpoint "https://api.fablo.pl/api/2/frisco.pl/products/query" )
+;due to unresolved CORS problem, i continue using mocked responce on local
+(def endpoint "http://127.0.0.1:8080/query.json")
+
+(defn search [phrase]
+  (go (let [responce (<! (http/get endpoint
+                          { :with-credentials? true
+                            :query-params { "results" 5
+                                            "search-string" phrase }} ))]
+        (aset (by-id "output") "value" (pr-str responce)))))
+
 (.addEventListener (by-id "query_form") "submit"
   (fn [event]
     (do
-      (js/alert (.-value (by-id "query")))
+      (search (aget (by-id "query") "value"))
       (.preventDefault event))))
 
